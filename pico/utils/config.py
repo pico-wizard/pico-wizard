c2adf83899b5bab089ba20a7bce4a204c9b11bf2
Probal Basak <probasak@in.ibm.com>
2020-11-27 16:43:11 +0530

    Config changes

---
+++ pico/utils/config.py
@@ -0,0 +1,17 @@
+import configparser
+from . import constants as conf
+
+
+class Config:
+    def __init__(self):
+        if self.instance is None:
+            self.config = configparser.ConfigParser()
+            read_files = self.config.read(conf.CONFIG_FILE_PATH)
+            if len(read_files) == 0:
+                raise ValueError('Could not find or open config file ' + conf.CONFIG_FILE_PATH)
+
+    def get_modules(self):
+        modules = self.config['GENERAL']['MODULES']
+        modules = [x.strip() for x in modules.split(",")]
+
+        return modules
