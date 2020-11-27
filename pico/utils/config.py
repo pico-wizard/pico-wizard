1ae909cfc12db70c78f215c5527cd75244b1926d
Probal Basak <probasak@in.ibm.com>
2020-11-27 18:07:24 +0530

    Fix

--- pico/utils/config.py
+++ pico/utils/config.py
@@ -8,18 +8,18 @@ class Config:
     config = None
 
     @staticmethod
-    def __get_cofig(self, config_section, config_name):
-        if self.config is None:
-            self.config = configparser.ConfigParser()
-            read_files = self.config.read(conf.CONFIG_FILE_PATH)
+    def __get_config(config_section, config_name):
+        if Config.config is None:
+            Config.config = configparser.ConfigParser()
+            read_files = Config.config.read(conf.CONFIG_FILE_PATH)
             if len(read_files) == 0:
                 raise ValueError('Could not find or open config file ' + conf.CONFIG_FILE_PATH)
 
-        return self.config[config_section][config_name]
+        return Config.config[config_section][config_name]
 
     @staticmethod
-    def get_modules(self):
-        modules_value = self.__get_config()
+    def get_modules():
+        modules_value = Config.__get_config()
         modules = [x.strip() for x in modules_value.split(",")]
 
         return modules
