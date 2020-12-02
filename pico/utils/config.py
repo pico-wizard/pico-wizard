faa6323f969e2be6d56112e66e84c3507c4660b1
Anupam Basak <anupam.basak27@gmail.com>
2020-12-03 01:09:38 +0530

    Ignore extra comma at last

--- pico/utils/config.py
+++ pico/utils/config.py
@@ -22,6 +22,6 @@ class Config:
     @staticmethod
     def get_modules():
         modules_value = Config.__get_config('GENERAL', 'MODULES')
-        modules = [x.strip() for x in modules_value.split(",")]
+        modules = [x.strip() for x in modules_value.split(",") if len(x) > 0]
 
         return modules
