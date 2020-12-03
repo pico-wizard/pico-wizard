252fe20be7185b7667d9db632e7ba1e1291c1c62
Anupam Basak <anupam.basak27@gmail.com>
2020-12-03 16:05:47 +0530

    Automatically import all Module subclasses

--- pico/modules/__init__.py
+++ pico/modules/__init__.py
@@ -1,8 +1,25 @@
-from .welcome import Welcome
-from .language import Language
-from .timezone import Timezone
-from .sim import Sim
-from .wifi import Wifi
-from .user import User
-from .personalization import Personalization
-from .finish import Finish
+import importlib
+import os
+from inspect import isclass
+from pathlib import Path
+
+from pico.module import Module4
+
+packageDir = Path(__file__).resolve().parent
+
+for moduleName in os.listdir(packageDir):
+    if os.path.isdir(os.path.join(packageDir, moduleName)):
+        for file in os.listdir(os.path.join(packageDir, moduleName)):
+            if file.endswith('.py'):
+                file = file[:-3]
+
+                try:
+                    module = importlib.import_module(f"{__name__}.{moduleName}.{file}")
+
+                    for clsName in dir(module):
+                        cls = getattr(module, clsName)
+
+                        if isclass(cls) and issubclass(cls, Module):
+                            globals()[clsName] = cls
+                except ModuleNotFoundError:
+                    pass
--- pico/modules/personalization/__init__.py
+++
@@ -1,1 +0,0 @@
-from .personalization import Personalization
