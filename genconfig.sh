b5d49c92bae8fa1bfbffc17f3fb9d2c340ced321
Open Webdesign Germany <62072464+OW-DG@users.noreply.github.com>
2021-06-22 14:25:37 +0200

    Update genconfig.sh

--- genconfig.sh
+++ genconfig.sh
@@ -1,1 +1,15 @@
- 
+mkdir temp-conf
+cd temp-conf
+echo "entering temporary directory"
+echo "getting config files from internet sources"
+wget "https://gitlab.manjaro.org/manjaro-arm/packages/community/plamo-gear/pico-wizard-git/-/raw/master/pico-wizard.conf" 
+wget "https://gitlab.manjaro.org/manjaro-arm/packages/community/plamo-gear/pico-wizard-git/-/raw/master/00-setup-autologin" "${pkgdir}/etc/pico-wizard/scripts.d/"
+wget "https://gitlab.manjaro.org/manjaro-arm/packages/community/plamo-gear/pico-wizard-git/-/raw/master/finish.hook" "${pkgdir}/etc/pico-wizard/scripts.d/" 
+wget "https://gitlab.manjaro.org/manjaro-arm/packages/community/plamo-gear/pico-wizard-git/-/raw/master/90-pico-wizard-autologin.conf" "${pkgdir}/etc/sddm.conf.d/"
+wget "https://gitlab.manjaro.org/manjaro-arm/packages/community/plamo-gear/pico-wizard-git/-/raw/master/pico-wizard-session" "${pkgdir}/usr/bin/"
+wget "https://gitlab.manjaro.org/manjaro-arm/packages/community/plamo-gear/pico-wizard-git/-/raw/master/pico-wizard-wrapper" "${pkgdir}/usr/bin/"
+wget "https://gitlab.manjaro.org/manjaro-arm/packages/community/plamo-gear/pico-wizard-git/-/raw/master/pico-wizard-cleanup" "${pkgdir}/usr/bin/"
+wget "https://gitlab.manjaro.org/manjaro-arm/packages/community/plamo-gear/pico-wizard-git/-/raw/master/pico-wizard-session.desktop" "${pkgdir}/usr/share/wayland-sessions/"
+wget "https://gitlab.manjaro.org/manjaro-arm/packages/community/plamo-gear/pico-wizard-git/-/raw/master/pico-wizard-cleanup.service" "${pkgdir}/usr/lib/systemd/system/"
+
+"/etc/pico-wizard/"
